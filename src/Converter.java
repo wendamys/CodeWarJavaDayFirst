b6d7fee0a9ebb7638bded127ce601159454d955e
wendamys <wendamys@yandex.ru>
2026-01-12 11:25:44 +0700

    add Converter.java

---
+++ src/Converter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class MyProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int sum = x + y;
+        String str = Integer.toString(x) + Integer.toString(y);
+        System.out.println(sum + "\n" + str);
+
+    }
+}
